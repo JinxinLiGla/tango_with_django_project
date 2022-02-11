5731c527299feb762e419de83028ffd8c616cf7b
Jinxin Li <2648095l@student.gla.ac.uk>
2022-02-11 13:36:30 +0000

    Fourth commit: Complete the exercises in Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
 
 # Register your models here.
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Page,PageAdmin)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('rango.urls')),
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
