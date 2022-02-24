f64432735a9026cc6ef0748e0bd5c11237dcd728
Jinxin Li <2648095l@student.gla.ac.uk>
2022-02-24 12:11:46 +0000

    Eighth commit: Complete the exercises in Chapter 9

--- rango/urls.py
+++ rango/urls.py
@@ -4,11 +4,15 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path(r'',views.index,name='index'),
+    # path(r'',views.index,name='index'),
     path(r'rango/about/', views.about, name='about'),
     path(r'rango/', views.index, name='index'),
     # path(r'rango/category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
     path(r'rango/category/<slug:category_name_slug>/',views.show_category, name='show_category'),
     path(r'rango/add_category/', views.add_category, name='add_category'),
     path(r'rango/category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
+    path(r'rango/register/', views.register, name='register'),
+    path(r'rango/login/', views.user_login, name='login'),
+    path(r'rango/restricted/', views.restricted, name='restricted'),
+    path(r'rango/logout/', views.user_logout, name='logout'),
 ]
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+LOGIN_URL = 'rango:login'
 
 
 # Quick-start development settings - unsuitable for production
@@ -100,8 +101,16 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
+    },
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
